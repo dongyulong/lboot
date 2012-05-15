458262e5439efca3892710802113f18914976e5a
Xiubo Li <superlibj@gmail.com>
2012-05-15 19:12:30 +0800

    Add lunch and croot function in setenv.sh script file.

--- include/config.h
+++ include/config.h
@@ -1,5 +1,35 @@
+#ifndef __LBOOT_CONFIG_
+#define __LBOOT_CONFIG_
 
+#include "variant.h"
 
+#ifdef UPLOOKING
 #define S3C2440_UPLOOKING
+#endif
+
+/* Memory initialize
+ *
+ * There 64M sdram, witch starting at 0x30000000,
+ * ending at 0x34000000;
+ *
+ * Kernel's start address is 0x30008000;
+ *
+ * Ramdisk's start address is 0x308000000;
+ *
+ * Bootloader's start address is 0x33f80000;
+ */
 
 
+
+#define CFG_ENV_SIZE			0xc000  /* Total Size of Environment Sector */
+#define CFG_MALLOC_LEN			(CFG_ENV_SIZE + 128*1024) 
+#define CFG_GBL_DATA_SIZE		128 /* size in bytes reserved for initial data */
+#define CONFIG_STACKSIZE_IRQ    (4*1024)    /* IRQ stack */
+#define CONFIG_STACKSIZE_FIQ    (4*1024)    /* FIQ stack */
+
+#define LBOOT_BASE				0x33f80000	/* lboot start address */
+#define TEXT_BASE				0x33d80000  /* */
+#define LINUX_KERNEL			0x30008000	/* linux kernel start address */
+#define RAMDISK					0x30800000	/* file system start address */
+
+#endif /* __LBOOT_CONFIG_ */
--- tool/script/setenv.sh
+++ tool/script/setenv.sh
@@ -1,4 +1,90 @@
 #!/bin/bash
 
-CURPATH=`pwd`
-export PATH=$CURPATH/tool/script:$PATH
+export TOPDIR=`pwd`
+
+LUNCH_MENU_CHOICES=([0]=TQ2440  [1]=Uplooking2440)
+
+function print_lunch_menu()
+{
+    local uname=$(uname)
+    echo
+    echo "You're building on" $uname
+    echo
+    echo "Lunch menu... pick a combo:"
+
+    local i=1
+    local choice
+    for choice in ${LUNCH_MENU_CHOICES[@]}
+    do
+        echo "     $i. $choice"
+        i=$(($i+1))
+    done
+
+    echo
+}
+
+function lunch()
+{
+    local answer
+
+    if [ "$1" ] ; then
+        answer=$1
+    else
+        print_lunch_menu
+        echo -n "Which would you like? [TQ2440] "
+        read answer
+    fi
+
+    local selection=
+
+    if [ -z "$answer" ]
+    then
+        selection=TQ2440
+    elif (echo -n $answer | grep -q -e "^[0-9][0-9]*$")
+    then
+        if [ $answer -le ${#LUNCH_MENU_CHOICES[@]} ]
+        then
+            selection=${LUNCH_MENU_CHOICES[$(($answer-1))]}
+        fi
+    elif (echo -n $answer | grep -q -e "^[^\-][^\-]*-[^\-][^\-]*$")
+    then
+        selection=$answer
+    fi
+
+    if [ -z "$selection" ]
+    then
+        echo
+        echo "Invalid lunch combo: $answer"
+        return 1
+    fi
+
+    local product=$(echo -n $selection | sed -e "s/-.*$//")
+
+	if [ "$selection" = "Uplooking2440" ]
+	then 
+#		echo "uplooking"
+		export UPLOOKING=1
+		echo "#define UPLOOKING" > include/variant.h
+	else
+		export UPLOOKING=0
+		echo " " > include/variant.h
+	fi
+
+}
+
+function croot()
+{
+	cd $TOPDIR
+}
+
+
+export lunch
+export croot
+
+
+export PATH=$TOPDIR/tool/script:$PATH
+
+
+
+
+
